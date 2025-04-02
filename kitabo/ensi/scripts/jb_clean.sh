149963fd6358abb99ec40fe9ac7323fb560aabc8
abikesa <abikesa.sh@gmail.com>
2025-04-02 11:34:32 -0400

    trim

---
+++ kitabo/ensi/scripts/jb_clean.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir -p _build/html/pdfs _build/html/figures _build/html/testbin _build/html/nis
+cp -r figures/* _build/html/figures/
+cp -r pdfs/* _build/html/pdfs/
+cp -r act1/app/* _build/html/act1/app
+cp -r testbin/* _build/html/testbin
+cp -r nis/* _build/html/nis
+
+
+
+
+
+
+
